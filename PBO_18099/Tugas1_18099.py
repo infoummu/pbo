b919bd87bb27db86cb7f6c105a1e2110a683e81c
viviyanti iksan sangadji <59107884+viviyanti@users.noreply.github.com>
2020-03-25 18:25:03 +0700

    Update Tugas1_18099.py

--- PBO_18099/Tugas1_18099.py
+++ PBO_18099/Tugas1_18099.py
@@ -5,14 +5,14 @@ _alamat = "jati"
 hobi_ = "berenang"
 __jenisKelamin = "p"
 tinggi1 = 163
-Berat = 50
+berat_ = 50
 print(nama)
 print(umurKu)
 print(_alamat)
 print(hobi_)
 print(__jenisKelamin)
 print(tinggi1)
-print(Berat)
+print(berat_)
 
 #contoh variabel lainnya :
 panjang = 10
