d04212e25148eb0255f2f1c6a04265e22f7ae8d1
qamarnur10 <62391343+qamarnur10@users.noreply.github.com>
2020-03-25 13:16:49 +0700

    Add files via upload

---
+++ Latihan 1 Variabel.python.py
@@ -0,0 +1,19 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=10
+>>> c=a+5
+>>> c
+10
+>>> Assalamualaikum="Assalamualaikum teman-teman"
+>>> Assalamualaikum
+'Assalamualaikum teman-teman'
+>>> a=20
+>>> b=4
+>>> c=a/b
+>>> c
+5.0
+>>> Semoga="Semoga selalu dalam lindungan Allah"
+>>> Semoga
+'Semoga selalu dalam lindungan Allah'
+>>> 
