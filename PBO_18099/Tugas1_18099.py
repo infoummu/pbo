fd964d7527694ad5a72aa5ddd7ba6e0026917fe2
viviyanti iksan sangadji <59107884+viviyanti@users.noreply.github.com>
2020-03-25 18:23:11 +0700

    Add files via upload
    Nama : Viviyanti Iksan Sangadji
    NPM : 18099
    Info : 3
    Semester : 4
    Tugas : PBO 1

---
+++ PBO_18099/Tugas1_18099.py
@@ -0,0 +1,21 @@
+#membuat variabel
+nama = "Vivi"
+umurKu = 20
+_alamat = "jati"
+hobi_ = "berenang"
+__jenisKelamin = "p"
+Tinggi1 = 163
+Berat = 50
+print(nama)
+print(umurKu)
+print(_alamat)
+print(hobi_)
+print(__jenisKelamin)
+print(Tinggi1)
+print(Berat)
+
+#contoh variabel lainnya :
+panjang = 10
+lebar = 3
+luas = panjang * lebar
+print(luas)
