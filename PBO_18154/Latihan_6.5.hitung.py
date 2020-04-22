482cb55e2805917c7b1e31083c2d061bcaf8737b
EkzaSaptawibowo154 <64135276+EkzaSaptawibowo154@users.noreply.github.com>
2020-04-22 19:49:32 +0700

    Create Latihan_6.5.hitung.py

---
+++ PBO_18154/Latihan_6.5.hitung.py
@@ -0,0 +1,6 @@
+def hitung():
+  umur=int(input("masukan umur kamu:"))
+  jj = umur * 369 * 24 * 60 * 60
+  print("kamu sudah hidup selama {jj}.detik")
+print ("selamat datang di program hitung detik umur")
+hitung()
