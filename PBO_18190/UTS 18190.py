ad59026fe2821316076cd1ff5097356a8dcbd2f0
diyansenen <62372611+diyansenen@users.noreply.github.com>
2020-06-15 21:43:43 -0700

    Add files via upload

---
+++ PBO_18190/UTS 18190.py
@@ -0,0 +1,18 @@
+class orang: 
+    def __init__(self, npm, nama):
+        self.np=npm
+        self.nm=nama
+    def tampilkan(self):
+        print ("1:","satu")
+        print ("2:","Dua")
+        print ("3:","tiga")
+        print ("0:","nol")
+        print ("4:","empat")
+        print ("5:","lima")
+        print ("npm:", self.nm,"\nnama:", self.np)
+class mahasiswa(orang):
+    pass
+ 
+orang=orang("diyana senen", "18190")
+orang.tampilkan()
+
